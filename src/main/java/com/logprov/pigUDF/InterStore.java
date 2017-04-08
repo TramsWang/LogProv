facbc591f8828dfda0dd9313c8e9115e6deb8a64
TramsWang <babyfish92@163.com>
2017-04-08 15:33:04 +1000

    Modify file read bug

--- src/main/java/com/logprov/pigUDF/InterStore.java
+++ src/main/java/com/logprov/pigUDF/InterStore.java
@@ -55,7 +55,6 @@ public class InterStore extends EvalFunc<Tuple>{
         lines = -1;
         ps_location = pm_url;
         hdfs = null;
-        this.pid = pid + '\n';
     }
 
     /**
@@ -78,6 +77,25 @@ public class InterStore extends EvalFunc<Tuple>{
             String operation = (String)input.get(1) + '\n';
             String varname = (String)input.get(2);
 
+            /* Get HDFS connection */
+            String hd_conf_dir = System.getenv("HADOOP_CONF_DIR");
+            if (null == hd_conf_dir)
+                throw new IOException("Environment variable 'HADOOP_CONF_DIR' not set!!");
+            Configuration conf = new Configuration();
+            conf.addResource(new Path(hd_conf_dir + "/core-site.xml"));
+            conf.addResource(new Path(hd_conf_dir + "/hdfs-site.xml"));
+            conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+            conf.set("fs.file.impl", LocalFileSystem.class.getName());
+            System.out.println("INTER:: Connecting to: " + conf.get("fs.defaultFS"));
+            hdfs = FileSystem.get(conf);
+
+            /* Open PID file */
+            if (!hdfs.exists(new Path(Config.PID_FILE)))
+                throw new IOException(String.format("PID file: '%s' not found!", Config.PID_FILE));
+            BufferedReader pidfile = new BufferedReader(new InputStreamReader(hdfs.open(new Path(Config.PID_FILE))));
+            pid = pidfile.readLine() + '\n';
+            pidfile.close();
+
             /* Send parameters and get storage path from PM */
             URL url = new URL(ps_location + Config.CONT_REQUIRE_DATA_STORAGE);
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
@@ -99,29 +117,10 @@ public class InterStore extends EvalFunc<Tuple>{
             in.close();
             System.out.println("INTER::<<<PATH>>>"+storage_path);
 
-            /* Get HDFS connection */
-            String hd_conf_dir = System.getenv("HADOOP_CONF_DIR");
-            if (null == hd_conf_dir)
-                throw new IOException("Environment variable 'HADOOP_CONF_DIR' not set!!");
-            Configuration conf = new Configuration();
-            conf.addResource(new Path(hd_conf_dir + "/core-site.xml"));
-            conf.addResource(new Path(hd_conf_dir + "/hdfs-site.xml"));
-            conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-            conf.set("fs.file.impl", LocalFileSystem.class.getName());
-            System.out.println("INTER:: Connecting to: " + conf.get("fs.defaultFS"));
-            hdfs = FileSystem.get(conf);
-
             /* Open output file */
             file = new PrintWriter(hdfs.create(new Path(storage_path)));
             //file = new PrintWriter(storage_path);
             lines = 0;
-
-            /* Open PID file */
-            if (!hdfs.exists(new Path(Config.PID_FILE)))
-                throw new IOException(String.format("PID file: '%s' not found!", Config.PID_FILE));
-            BufferedReader pidfile = new BufferedReader(new InputStreamReader(hdfs.open(new Path(Config.PID_FILE))));
-            pid = pidfile.readLine() + '\n';
-            pidfile.close();
         }
 
         Tuple t = TupleFactory.getInstance().newTuple();
